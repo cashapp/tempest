31741135b4cc562d0e0d1cee3290660c2d17f1b7
Chris Ryan <chrisryan@squareup.com>
2022-03-21 14:32:38 +1000

    Adds the correct sqlite4java dep when running on M1 Macs

--- tempest-testing-jvm/build.gradle.kts
+++ tempest-testing-jvm/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
-  kotlin("jvm")
-  `java-library`
+    kotlin("jvm")
+    `java-library`
 }
 
 dependencies {
-  api(project(":tempest-testing"))
-  implementation(project(":tempest-testing-internal"))
-  implementation(Dependencies.awsDynamodbLocal)
-  implementation(Dependencies.kotlinStdLib)
+    api(project(":tempest-testing"))
+    implementation(project(":tempest-testing-internal"))
+    implementation(Dependencies.awsDynamodbLocal)
+    implementation(Dependencies.kotlinStdLib)
 
-  testImplementation(Dependencies.assertj)
-  testImplementation(Dependencies.junitApi)
-  testImplementation(Dependencies.junitEngine)
+    if (org.apache.tools.ant.taskdefs.condition.Os.isArch("aarch64")) {
+        implementation("io.github.ganadist.sqlite4java:libsqlite4java-osx-aarch64:1.0.392")
+    }
+
+    testImplementation(Dependencies.assertj)
+    testImplementation(Dependencies.junitApi)
+    testImplementation(Dependencies.junitEngine)
 }
--- tempest2-testing-jvm/build.gradle.kts
+++ tempest2-testing-jvm/build.gradle.kts
@@ -9,6 +9,10 @@ dependencies {
   implementation(Dependencies.awsDynamodbLocal)
   implementation(Dependencies.kotlinStdLib)
 
+  if (org.apache.tools.ant.taskdefs.condition.Os.isArch("aarch64")) {
+    implementation("io.github.ganadist.sqlite4java:libsqlite4java-osx-aarch64:1.0.392")
+  }
+
   testImplementation(Dependencies.assertj)
   testImplementation(Dependencies.junitApi)
   testImplementation(Dependencies.junitEngine)
