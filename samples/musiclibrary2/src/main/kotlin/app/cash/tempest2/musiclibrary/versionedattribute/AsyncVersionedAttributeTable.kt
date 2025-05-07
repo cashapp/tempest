ad79d879fb64d3ac8c27ce9f61027a473b319b80
Eric Loewenthal <ericloe@squareup.com>
2025-05-07 00:25:11 -0700

    Add parallel withResult test.

--- samples/musiclibrary2/src/main/kotlin/app/cash/tempest2/musiclibrary/versionedattribute/AsyncVersionedAttributeTable.kt
+++ samples/musiclibrary2/src/main/kotlin/app/cash/tempest2/musiclibrary/versionedattribute/AsyncVersionedAttributeTable.kt
@@ -2,7 +2,52 @@ package app.cash.tempest2.musiclibrary.versionedattribute
 
 import app.cash.tempest2.AsyncInlineView
 import app.cash.tempest2.AsyncLogicalTable
+import app.cash.tempest2.Attribute
+import java.time.Instant
 
 interface AsyncVersionedAttributeTable : AsyncLogicalTable<VersionedAttributeItem> {
   val attributes: AsyncInlineView<VersionedAttribute.Key, VersionedAttribute>
+  val parallelAttributes: AsyncInlineView<ParallelVersionedAttribute.Key, ParallelVersionedAttribute>
+}
+
+/**
+ * Stores the current version and time stamps as it writes
+ * the next version to verify parallel write behavior.
+ */
+data class ParallelVersionedAttribute(
+  val partition_key: String,
+  val description: String,
+
+  val created_at: Instant? = null,
+  val updated_at: Instant? = null,
+
+  val one_version: Long? = null,
+  val one_created_at: Instant? = null,
+  val one_updated_at: Instant? = null,
+
+  val two_version: Long? = null,
+  val two_created_at: Instant? = null,
+  val two_updated_at: Instant? = null,
+
+  val three_version: Long? = null,
+  val three_created_at: Instant? = null,
+  val three_updated_at: Instant? = null,
+
+  val four_version: Long? = null,
+  val four_created_at: Instant? = null,
+  val four_updated_at: Instant? = null,
+
+  val version: Long? = null,
+) {
+  @Attribute(prefix = "PARALLEL_INFO_")
+  val sort_key: String = ""
+
+  @Transient
+  val key = Key(partition_key)
+
+  data class Key(
+    val partition_key: String
+  ) {
+    val sort_key: String = ""
+  }
 }
--- tempest2/src/test/kotlin/app/cash/tempest2/TestUtils.kt
+++ tempest2/src/test/kotlin/app/cash/tempest2/TestUtils.kt
@@ -5,27 +5,41 @@ import kotlinx.coroutines.runBlocking
 import java.time.Clock
 import java.time.Instant
 import java.time.ZoneId
+import java.util.Date
+import java.util.concurrent.atomic.AtomicLong
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 // Exists to silence Intellij warning "Junit test should return Unit".
 fun runBlockingTest(block: suspend CoroutineScope.() -> Unit) {
   runBlocking(block = block)
 }
 
-class FakeClock : Clock() {
-  var epochNow = 452_001_600_000
+/**
+ * @param tickOnNow ticks the clock forward this amount every time [instant] is called, result includes the updated time.
+ *
+ * Test should use [getInstant] et al. to read the current time without ticking the clock.
+ */
+class FakeClock(val tickOnNow: Duration = 0.seconds) : Clock() {
+  private var epochNow = AtomicLong(452_001_600_000)
 
   fun add(duration: Duration) {
-    epochNow += duration.inWholeMilliseconds
+    epochNow.addAndGet(duration.inWholeMilliseconds)
   }
 
+  fun getInstant() = Instant.ofEpochMilli(epochNow.get())
+
+  fun getDate() = Date.from(Instant.ofEpochMilli(epochNow.get()))
+
+  fun minusTicks(ticks: Int) = Instant.ofEpochMilli(epochNow.addAndGet(-(ticks * tickOnNow.inWholeMilliseconds)))
+
   override fun instant(): Instant {
-    return Instant.ofEpochMilli(epochNow)
+    return Instant.ofEpochMilli(epochNow.addAndGet(tickOnNow.inWholeMilliseconds))
   }
 
   override fun withZone(zone: ZoneId?): Clock =
     TODO("Not yet implemented")
 
   override fun getZone(): ZoneId =
-    TODO("Not yet implemented")
+    ZoneId.systemDefault()
 }
