2c6703701b1b4e6a74e180d666fd0d9d9855c609
Eric Loewenthal <ericloe@squareup.com>
2025-07-11 15:14:31 -0700

    Fix Date TempestDateAttributeConverter dropping millisecond precision

--- tempest2/src/main/kotlin/app/cash/tempest2/extensions/TempestDateAttributeConverter.kt
+++ tempest2/src/main/kotlin/app/cash/tempest2/extensions/TempestDateAttributeConverter.kt
@@ -4,12 +4,12 @@ import software.amazon.awssdk.enhanced.dynamodb.AttributeConverter
 import software.amazon.awssdk.enhanced.dynamodb.AttributeConverterProvider
 import software.amazon.awssdk.enhanced.dynamodb.AttributeValueType
 import software.amazon.awssdk.enhanced.dynamodb.EnhancedType
+import software.amazon.awssdk.enhanced.dynamodb.internal.converter.attribute.InstantAsStringAttributeConverter
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import java.util.Date
 
 @Suppress("UNCHECKED_CAST")
 class TempestDateAttributeConverterProvider : AttributeConverterProvider {
-
   override fun <T : Any> converterFor(enhancedType: EnhancedType<T>): AttributeConverter<T>? =
     when (enhancedType.rawClass()) {
       Date::class.java -> DATE_ATTRIBUTE_CONVERTER as AttributeConverter<T>
@@ -22,17 +22,19 @@ class TempestDateAttributeConverterProvider : AttributeConverterProvider {
 }
 
 class TempestDateAttributeConverter : AttributeConverter<Date> {
-  override fun transformFrom(input: Date): AttributeValue =
-    AttributeValue.builder().s(input.toString()).build()
 
-  @Suppress("DEPRECATION")
-  override fun transformTo(input: AttributeValue): Date =
-    Date(Date.parse(input.s()))
+  override fun transformFrom(input: Date): AttributeValue = INSTANT_CONVERTER.transformFrom(input.toInstant())
 
+  override fun transformTo(input: AttributeValue): Date = Date.from(INSTANT_CONVERTER.transformTo(input))
 
-  override fun type(): EnhancedType<Date> =
-    EnhancedType.of(Date::class.java)
+  override fun type(): EnhancedType<Date> = EnhancedType.of(Date::class.java)
 
-  override fun attributeValueType(): AttributeValueType =
-    AttributeValueType.S
+  override fun attributeValueType(): AttributeValueType = AttributeValueType.S
+
+  companion object {
+    /**
+     * Use instant converter for ISO 8601 compatibility when converting Date
+     */
+    private val INSTANT_CONVERTER = InstantAsStringAttributeConverter.create()
+  }
 }
