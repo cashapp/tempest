b81c7499b433dcbd53ad96141c497f972aa33c1f
Eric Loewenthal <ericloe@squareup.com>
2025-10-19 15:51:37 -0700

    Use ImmutableTableSchemaParams in fromImmutableClass

--- tempest2/src/main/kotlin/app/cash/tempest2/internal/TableSchemaFactory.kt
+++ tempest2/src/main/kotlin/app/cash/tempest2/internal/TableSchemaFactory.kt
@@ -2,6 +2,7 @@ package app.cash.tempest2.internal
 
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
 import software.amazon.awssdk.enhanced.dynamodb.mapper.BeanTableSchemaParams
+import software.amazon.awssdk.enhanced.dynamodb.mapper.ImmutableTableSchemaParams
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbImmutable
 import java.lang.invoke.MethodHandles
@@ -14,16 +15,17 @@ object TableSchemaFactory {
   /**
    * Compute the TableSchema, which a moderately expensive operation, and cache the result.
    */
+  @Suppress("UNCHECKED_CAST")
   fun <T> create(clazz: Class<*>): TableSchema<T> {
     return schemas.getOrPut(clazz) {
       // This is ported from TableSchema.fromClass which was the previous implementation.
-      // The modification is to use BeanTableSchemaParams in fromBean:
-      //   Generally, this method should be preferred over fromBean(Class) because it allows
+      // The modification is to use BeanTableSchemaParams and ImmutableTableSchemaParams:
+      //   Generally, this method should be preferred over from*(Class) because it allows
       //   you to use a custom MethodHandles.Lookup instance, which is necessary when your
       //   application runs in an environment where your application code and dependencies
       //   like the AWS SDK for Java are loaded by different classloaders.
       if (clazz.getAnnotation<DynamoDbImmutable>(DynamoDbImmutable::class.java) != null) {
-        TableSchema.fromImmutableClass(clazz)
+        TableSchema.fromImmutableClass(ImmutableTableSchemaParams.builder(clazz).lookup(MethodHandles.lookup()).build())
       } else if (clazz.getAnnotation<DynamoDbBean>(DynamoDbBean::class.java) != null) {
         TableSchema.fromBean(BeanTableSchemaParams.builder(clazz).lookup(MethodHandles.lookup()).build())
       } else {
